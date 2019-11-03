3ee42be4fd428768b30eb37b29354645367d184f
shakestest <57329530+shakestest@users.noreply.github.com>
2019-11-03 21:57:42 +0200

    testnet

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h> 
+#include <math.h>
+
+int main()
+{
+	double v0, a, t, V;
+
+	//Введення початкових даних
+	printf("Input v0: ");
+	scanf("%if", &v0);
+	printf("Input a: ");
+	scanf("%if", &a);
+	printf("Input t: ");
+	scanf("%if", &t)
+		V = v0 + a * t;
+	printf("V=%if\n", V);
+	system("pause");
+	return 0
+}
